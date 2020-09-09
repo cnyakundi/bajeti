9ec397427b6d23dee4fbc87ad4533df7e86b482b
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-09 17:43:05 +0300

    Initial commit

---
+++ javascript/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    function Expense(title, amount) {
+        this.title = title;
+        this.amount = amount;
+    }
+  
+    var totalExpenses;
+  
+    var expenses;
+  
+    var budget;
+  
+    var balance;
+})
