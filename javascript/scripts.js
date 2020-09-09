2432d72fa92eeeaf97f436edfb96f843c6d3c78b
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-09 17:47:22 +0300

    Add function to the budget form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,3 +12,18 @@ $(document).ready(function () {
   
     var balance;
 })
+
+$("#budget-form").submit(function (e) {
+    e.preventDefault();
+    var budgetEntered = parseInt($("#budget").val());
+    expenses = [];
+    budget = budgetEntered;
+    balance = budget;
+    $("#balance-amount").empty();
+    $("#balance-amount").append(balance);
+    $("#budget-amount").empty();
+    $("#budget-amount").append(budget);
+    $("#expenses-amount").empty();
+    $("#expenses-amount").append(0);
+    $("#budget").val("");
+  });
